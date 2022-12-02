873a402d0b8691ebe480d3c561bac6196b8ffe62
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2022-12-03 00:51:04 +0100

    Update settings.py

--- apjot/settings.py
+++ apjot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-as&l9#gl@(wqa%^^h8t25n-+#t8zfa8^ow5cl(js9llayllcay'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['my-apjot-production.up.railway.app']
 
