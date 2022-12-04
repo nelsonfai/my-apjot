9a7104063c92158476293533cde192fe20307022
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2022-12-05 00:25:11 +0100

    Update settings.py

--- apjot/settings.py
+++ apjot/settings.py
@@ -157,4 +157,4 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'fainelson855@gmail.com'
 EMAIL_HOST_PASSWORD = 'yeodnofweiirvltw'
 
-CSRF_TRUSTED_ORIGINS = ["https://my-apjot-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://my-apjot-production.up.railway.app","https://www.apjot.blog"]
