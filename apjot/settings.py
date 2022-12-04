9043014a368cf02a23c78c12e3c3034e1bd3010c
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2022-12-04 01:56:05 +0100

    Update settings.py

--- apjot/settings.py
+++ apjot/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'apjot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -99,7 +99,7 @@ DATABASES = {
         'PORT':'7159',    
         }
 }
-
+"""
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
