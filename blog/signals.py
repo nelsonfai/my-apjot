553e08405ee8e4cb0ad68daff307216428501c7c
Nelson fai <101453696+nelsonfai@users.noreply.github.com>
2023-05-24 10:37:56 +0200

    API UPDATE

--- blog/signals.py
+++ blog/signals.py
@@ -6,35 +6,40 @@ print('IN barca')
 @receiver(post_save, sender=Articles)
 def article_created(sender, instance, created, **kwargs):
     if created:
-        # Perform actions or logic when a new article is created
-        # For example:
         print("New article created:", instance.title)
-        # You can include any additional actions you need here
         if instance.post_medium:
             title = instance.title
             content = instance.body
-            tags = list(instance.tags.names())  # Convert queryset to list
+            tags = list(instance.tags.names())
+            author_id = "1119602260574705623"  # Replace with the actual authorId
             access_token = "2acb799cb1df1634463cf85d113ffaae9ec28e299eb20ad5675f34f7109f2abf5"
-            response = publish_to_medium(title, content, tags, access_token)
+            response = publish_to_medium(title, content, tags, author_id, access_token)
             print(response)
 
-def publish_to_medium(title, content, tags, access_token):
-    url = "https://api.medium.com/v1/users/self/posts"
-
+def publish_to_medium(title, content, tags, author_id, access_token):
+    url = f"https://api.medium.com/v1/users/{author_id}/posts"
     headers = {
-        "Content-Type": "application/json",
-        "Authorization": f"Bearer {access_token}",
+        'Host': 'api.medium.com',
+        'Authorization': f'Bearer {access_token}',
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Accept-Charset': 'utf-8'
     }
-
     data = {
         "title": title,
         "contentFormat": "html",
         "content": content,
         "tags": tags,
-        "publishStatus": "draft",
+        "publishStatus": "draft"
     }
 
     response = requests.post(url, headers=headers, json=data)
-    response.raise_for_status()  # Raise an exception if the request was unsuccessful
+
+    try:
+        response.raise_for_status()
+        print("Article published successfully:", response.json())
+    except requests.exceptions.HTTPError as error:
+        print("Error publishing article:", error)
+        print("Response content:", response.content)
 
     return response.json()
